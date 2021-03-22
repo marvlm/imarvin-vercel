0887b2e8832e17b1dd6b0c99b36a74debab20fc2
Marvin <marv.wi3@gmail.com>
2021-03-22 13:52:52 +0100

    Added logo to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,26 @@
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
+import Logo from '@/data/logo.svg'
+import DarkLogo from '@/data/logo-dark.svg'
+import { useTheme } from 'next-themes'
 
 export default function Footer() {
+  const { theme, setTheme } = useTheme()
   return (
     <footer>
       <div className="flex flex-col items-center mt-16">
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          {theme === 'dark' ? (
+            <div className="mr-3 w-8/12">
+              <DarkLogo />
+            </div>
+          ) : (
+            <div className="r-3 w-8/12">
+              <Logo />
+            </div>
+          )}
+        </div>
         <div className="flex mb-3 space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
           <SocialIcon kind="github" href={siteMetadata.github} size="6" />
